b54476c8d9b4f51df8718561ed29d4a68f813ef9
Anthony Spence <anthonyspence08091973@gmail.com>
2023-08-30 14:35:04 -0700

    Feature 6 Complete

--- receipts/urls.py
+++ receipts/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import receipt_list
+from receipts.views import receipt_list
 
-app_name = "receipts"
 
 urlpatterns = [
     path("", receipt_list, name="home"),
